7b137c8c0f22ae390455d4335cf802da8dda0394
yukana <tianyi.li2@gmail.com>
2017-03-06 13:33:49 -0800

    finish the style of search.html

--- HW4/js/login.js
+++ HW4/js/login.js
@@ -1,19 +1,10 @@
 // Initialize Firebase
-// var config = {
-//   apiKey: "AIzaSyBTFeLiA_68jNfN2AQN_GIudQSZd1mKalY",
-//   authDomain: "myproj134b.firebaseapp.com",
-//   databaseURL: "https://myproj134b.firebaseio.com",
-//   storageBucket: "myproj134b.appspot.com",
-//   messagingSenderId: "746940840004"
-// };
-
-// Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBSqnmEYkvUCT084y_NPpJeL9-TofpOsq4",
-    authDomain: "cse134b-all.firebaseapp.com",
-    databaseURL: "https://cse134b-all.firebaseio.com",
-    storageBucket: "",
-    messagingSenderId: "295582276921"
+  apiKey: "AIzaSyBTFeLiA_68jNfN2AQN_GIudQSZd1mKalY",
+  authDomain: "myproj134b.firebaseapp.com",
+  databaseURL: "https://myproj134b.firebaseio.com",
+  storageBucket: "myproj134b.appspot.com",
+  messagingSenderId: "746940840004"
 };
 firebase.initializeApp(config);
 
